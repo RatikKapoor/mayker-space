4064dd322a7b221a07df9a96146c94b8944b7c4e
SamsonHua <samson.hua@ucalgary.ca>
2021-02-13 15:09:05 -0700

    Please don't break

--- src/components/BaseMap.tsx
+++ src/components/BaseMap.tsx
@@ -1,20 +1,20 @@
-import React, { useEffect } from "react";
-import mapboxgl from "mapbox-gl";
-import "./BaseMap.scss";
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import './BaseMap.css';
 
 const BaseMap = () => {
-  mapboxgl.accessToken = 'pk.eyJ1Ijoic2Ftc29uaCIsImEiOiJja2w0OG15MXkwYnlwMnZxeHJveHNidWxvIn0.QSg83CVaQ3JwLPJJzMDp6w';
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic2Ftc29uaCIsImEiOiJja2w0OG15MXkwYnlwMnZxeHJveHNidWxvIn0.QSg83CVaQ3JwLPJJzMDp6w';
 
-  useEffect(() => {
-    new mapboxgl.Map({
-      container: "mapContainer",
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [51.1, -114],
-      zoom: 9,
-    });
-  }, []);
+    useEffect(() => {
+        new mapboxgl.Map({
+            container: 'mapContainer',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-113.92, 50.9],
+            zoom: 14,
+        });
+    }, []);
 
-  return <div id="mapContainer" className="map"></div>;
+    return <div id="mapContainer" className="map"></div>;
 };
 
 export default BaseMap;
