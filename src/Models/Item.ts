6e03e9a9ccfdf1c0fdb3c18d00a4c26f0e2c8f26
Robert Brown <brown.rjb75@gmail.com>
2021-02-13 14:24:01 -0700

    added itemview

--- src/Models/Item.ts
+++ src/Models/Item.ts
@@ -1,13 +1,19 @@
 export default class Item {
     /**
-     * A class for holding a tool that is avalible for renting
+     * A class for holding a tool that is available for renting
      */
 
     id: string;
     name: string;
+    category: string;
+    description: string;
+    imageUrl: string;
 
-    constructor(id: string, name: string) {
+    constructor(id: string, name: string, category: string, description: string, imageUrl: string) {
         this.id = id;
         this.name = name;
+        this.category = category;
+        this.description = description;
+        this.imageUrl = imageUrl;
     }
 }
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -3,21 +3,37 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
+import Item from '../Models/Item';
+import ItemView from '../components/ItemView';
+import ItemRequest from '../components/ItemRequest';
+
+const testItem = new Item(
+    'ABC123',
+    'Arduino Uno',
+    'Microcontroller',
+    'I have an Arduino Uno R3 available for use. Would only be willing to give it up for a few weeks.',
+    'https://drscdn.500px.org/photo/58131206/q%3D80_m%3D2000/v2?sig=d6e8d355df86733d2430b9a42c9af7718b324f3b49ff78e8983eb60673770553',
+);
+
 const Tab1: React.FC = () => {
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
-                    <IonTitle>Tab 1</IonTitle>
+                    <IonTitle>Available Items</IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
                 <IonHeader collapse="condense">
                     <IonToolbar>
-                        <IonTitle size="large">Tab 1</IonTitle>
+                        <IonTitle size="large">Available Items</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                <ExploreContainer name="Tab 1 page" />
+                <ItemRequest />
+                <ItemView item={testItem} />
+                <ItemView item={testItem} />
+                <ItemView item={testItem} />
+                <ItemView item={testItem} />
             </IonContent>
         </IonPage>
     );
