47af031bb04ac89b3a3009aaf7d05bc1be1549fa
Ratik Kapoor <ratikrk@gmail.com>
2021-02-13 12:50:57 -0700

    Fix linting issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
