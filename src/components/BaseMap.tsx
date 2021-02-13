ab89116105ba22b8955a21d3912fe083e3ebcf0f
SamsonHua <samson.hua@ucalgary.ca>
2021-02-13 15:36:58 -0700

    Mapbox API Confirmed

--- src/components/BaseMap.tsx
+++ src/components/BaseMap.tsx
@@ -6,14 +6,30 @@ const BaseMap = () => {
     mapboxgl.accessToken = 'pk.eyJ1Ijoic2Ftc29uaCIsImEiOiJja2w0OG15MXkwYnlwMnZxeHJveHNidWxvIn0.QSg83CVaQ3JwLPJJzMDp6w';
 
     useEffect(() => {
-        new mapboxgl.Map({
+        const map = new mapboxgl.Map({
             container: 'mapContainer',
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [-113.92, 50.9],
             zoom: 14,
         });
-    }, []);
+        //Set navigation controls
+        const nav = new mapboxgl.NavigationControl();
+        map.addControl(nav, 'top-right');
+        //Set marker to Ratik's House
+        const marker = new mapboxgl.Marker()
+            //This will set the marker
+            .setLngLat([-113.92, 50.9])
+            .addTo(map);
+        //Geolocate
+        const geolocate = new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true,
+            },
+            trackUserLocation: true,
+        });
 
+        map.addControl(geolocate, 'top-right');
+    }, []);
     return <div id="mapContainer" className="map"></div>;
 };
 
