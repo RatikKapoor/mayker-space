0b184d5d00493212cfdd012298bec2dc0ce3aa8b
SamsonHua <samson.hua@ucalgary.ca>
2021-02-13 15:09:05 -0700

    Please don't break

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -2,23 +2,12 @@ import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.scss';
+import BaseMap from '../components/BaseMap';
 
 const Tab2: React.FC = () => {
     return (
         <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Tab 2</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Tab 2</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <ExploreContainer name="Tab 2 page" />
-            </IonContent>
+            <BaseMap />
         </IonPage>
     );
 };
