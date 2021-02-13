6f94596a00b3d46ae7013b3287889ecfdce2cfe8
Robert Brown <brown.rjb75@gmail.com>
2021-02-13 12:55:32 -0700

    added item and route classes

---
+++ src/Models/Item.ts
@@ -0,0 +1,17 @@
+export default class Item {
+
+    /**
+     * A class for holding a tool that is avalible for renting
+     */
+
+    id: string;
+    name: string;
+
+    constructor(
+        id: string,
+        name: string
+    ) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ src/Models/Route.ts
@@ -0,0 +1,20 @@
+export default class Route {
+
+    /**
+     * A class for holding a a route between lender and lender
+     */
+
+    id: string;
+    origin: string;
+    destination: string;
+
+    constructor(
+        id: string,
+        origin: string,
+        destination: string
+    ) {
+        this.id = id;
+        this.origin = origin;
+        this.destination = destination;
+    }
+}
