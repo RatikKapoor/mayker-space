8731b1c9093fe25265b6aae86b629f95d812eb67
Robert Brown <brown.rjb75@gmail.com>
2021-02-13 14:24:01 -0700

    added itemview

---
+++ src/components/ItemRequest.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { IonDatetime, IonInput, IonItem, IonLabel, IonList } from '@ionic/react';
+import './ItemView.css';
+
+const ItemView: React.FC = () => {
+    return (
+        <IonList>
+            <IonItem>
+                <IonLabel>Start Date</IonLabel>
+                <IonDatetime value="1990-02-19" placeholder="Select Date"></IonDatetime>
+            </IonItem>
+            <IonItem>
+                <IonLabel>End Date</IonLabel>
+                <IonDatetime value="1990-02-19" placeholder="Select Date"></IonDatetime>
+            </IonItem>
+            <IonItem>
+                <IonInput placeholder="Location"></IonInput>
+            </IonItem>
+        </IonList>
+    );
+};
+
+export default ItemView;
---
+++ src/components/ItemView.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import './ItemView.css';
+import Item from '../Models/Item';
+
+interface ItemProps {
+    item: Item;
+}
+
+const ItemView: React.FC<ItemProps> = (props: ItemProps) => {
+    return (
+        <IonCard>
+            <img src={props.item.imageUrl}></img>
+            <IonCardHeader>
+                <IonCardSubtitle>{props.item.category}</IonCardSubtitle>
+                <IonCardTitle>{props.item.name}</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>{props.item.description}</IonCardContent>
+            <IonButton>Request</IonButton>
+        </IonCard>
+    );
+};
+
+export default ItemView;
