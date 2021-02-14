9e6061a526aaf934c9d50f609ec61b13706c9ef9
Ratik Kapoor <ratikrk@gmail.com>
2021-02-13 18:13:45 -0700

    Add loading and private routing

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import app from '../Models/Firebase';
+import { loadingComponent } from './Loading';
+
+interface PrivateRouteInterface {
+    component: React.FC;
+    path: string;
+    exact: boolean;
+}
+
+export const PrivateRoute: React.FC<{ component: React.FC; path: string; exact: boolean }> = (
+    props: PrivateRouteInterface,
+) => {
+    const [loading, setLoadingState] = useState(true);
+    const [authed, setAuthed] = useState<boolean | null>();
+
+    app.auth().onAuthStateChanged((user) => {
+        setAuthed(Boolean(user));
+        setLoadingState(authed == null);
+    });
+
+    console.log(authed);
+    const loadedComponent = authed ? (
+        <Route path={props.path} exact={props.exact} component={props.component} />
+    ) : (
+        <Redirect to="/login" exact={true} />
+    );
+    return loading ? loadingComponent : loadedComponent;
+};
