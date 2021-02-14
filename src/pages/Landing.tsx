5cb5df32c1d530b517134b35ebd6a07cc0a493fc
Robert Brown <brown.rjb75@gmail.com>
2021-02-13 20:01:19 -0700

    added basic landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,8 +1,29 @@
-import { IonContent } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTabBar, IonTabButton, IonText, IonTitle, IonLabel } from '@ionic/react';
 import React from 'react';
+import './Landing.scss';
 
 const Landing: React.FC = () => {
-    return <IonContent />;
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonTabBar>
+                    <IonTabButton>
+                        <IonLabel>About Us</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton>
+                        <IonLabel>Services</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton>
+                        <IonLabel>Contact Us</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton>
+                        <IonLabel>Sign Up</IonLabel>
+                    </IonTabButton>
+                </IonTabBar>
+            </IonHeader>
+            <IonContent className="homeContainer"></IonContent>
+        </IonPage>
+    );
 };
 
 export default Landing;
