5b370fe712ff20f1e52bc9cf4cf0d629ec1772c7
Ratik Kapoor <ratikrk@gmail.com>
2021-02-14 09:48:38 -0700

    Fix Map, BaseMent

--- src/components/BaseMap.tsx
+++ src/components/BaseMap.tsx
@@ -1,47 +1,96 @@
-import React, { useEffect } from 'react';
-import mapboxgl from 'mapbox-gl';
-import Directions from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
-import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+// import mapboxgl from 'mapbox-gl';
+// import Directions from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import MapGL, { GeolocateControl, Marker, Popup } from 'react-map-gl';
+// import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
 import './BaseMap.scss';
 import Route from '../Models/Route';
+import Pin from './Pin';
+import { IonText } from '@ionic/react';
 
 interface MapProps {
     route: Route;
 }
 
-const BaseMap: React.FC<MapProps> = (props: MapProps) => {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic2Ftc29uaCIsImEiOiJja2w0OG15MXkwYnlwMnZxeHJveHNidWxvIn0.QSg83CVaQ3JwLPJJzMDp6w';
-    useEffect(() => {
-        console.log('printing map' + props.route.originlat + ', ' + props.route.originlon);
-        const map = new mapboxgl.Map({
-            container: 'mapContainer',
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [-117.1425, 32.63638889],
-            zoom: 14,
-        });
-        //Set navigation controls
-        const nav = new mapboxgl.NavigationControl();
-        map.addControl(nav, 'top-right');
+const geolocateStyle = {
+    top: 0,
+    left: 0,
+    margin: 10,
+};
 
-        //Geolocate
-        const geolocate = new mapboxgl.GeolocateControl({
-            positionOptions: {
-                enableHighAccuracy: true,
-            },
-            trackUserLocation: true,
-        });
-        map.addControl(geolocate, 'top-right');
-        const directions = new Directions({
-            accessToken: 'pk.eyJ1Ijoic2Ftc29uaCIsImEiOiJja2w0OG15MXkwYnlwMnZxeHJveHNidWxvIn0.QSg83CVaQ3JwLPJJzMDp6w',
-            unit: 'metric',
-            profile: 'mapbox/driving',
-            interactive: false,
-        });
-        directions.setOrigin([props.route.originlat, props.route.originlon]);
-        directions.setDestination([props.route.destinationlat, props.route.destinationlon]);
-        map.addControl(directions, 'top-left');
-    }, []);
-    return <div id="mapContainer" className="map"></div>;
+const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
+  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
+  C20.1,15.8,20.2,15.8,20.2,15.7z`;
+
+const SIZE = 20;
+
+const positionOptions = { enableHighAccuracy: true };
+
+const BaseMap: React.FC<MapProps> = (props: MapProps) => {
+    const accessToken = 'pk.eyJ1Ijoic2Ftc29uaCIsImEiOiJja2w0OG15MXkwYnlwMnZxeHJveHNidWxvIn0.QSg83CVaQ3JwLPJJzMDp6w';
+    const [popupInfo, setPopupInfo] = useState(null);
+
+    const [viewport, setViewport] = useState({
+        // width: au,
+        // height: 400,
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 1,
+    });
+    const handleViewportChange = useCallback((newViewport) => setViewport(newViewport), []);
+    return (
+        <div style={{ height: '100vh' }}>
+            <MapGL
+                {...viewport}
+                width="100%"
+                height="100%"
+                onViewportChange={handleViewportChange}
+                mapboxApiAccessToken={accessToken}
+                mapStyle="mapbox://styles/mapbox/streets-v11"
+            >
+                {props.route.originlat && props.route.originlon && (
+                    <Marker key={`marker-1`} longitude={props.route.originlon} latitude={props.route.originlat}>
+                        <svg
+                            height={SIZE}
+                            viewBox="0 0 24 24"
+                            style={{
+                                cursor: 'pointer',
+                                fill: '#d00',
+                                stroke: 'none',
+                                transform: `translate(${-SIZE / 2}px,${-SIZE}px)`,
+                            }}
+                        >
+                            <path d={ICON} />
+                        </svg>
+                    </Marker>
+                )}
+                <Marker key={`marker-1`} longitude={props.route.destinationlon} latitude={props.route.destinationlat}>
+                    <svg
+                        height={SIZE}
+                        viewBox="0 0 24 24"
+                        style={{
+                            cursor: 'pointer',
+                            fill: '#d00',
+                            stroke: 'none',
+                            transform: `translate(${-SIZE / 2}px,${-SIZE}px)`,
+                        }}
+                    >
+                        <path d={ICON} />
+                    </svg>
+                </Marker>
+                <GeolocateControl
+                    style={geolocateStyle}
+                    positionOptions={positionOptions}
+                    // trackUserLocation
+                    auto
+                    onGeolocate={(e) => {
+                        console.log(e);
+                    }}
+                />
+            </MapGL>
+        </div>
+    );
 };
 
 export default BaseMap;
